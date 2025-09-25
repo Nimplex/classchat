9c5ac217f58dc06e207e7acaba3902e2f3f0c4f4
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-09-25 10:43:19 +0200

    Add ListingController and ListingService

---
+++ src/Service/ListingService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service;
+
+use App\Model\Listing;
+
+class ListingService
+{
+    private Listing $listingModel;
+
+    public function __construct(Listing $listingModel)
+    {
+        $this->listingModel = $listingModel;
+    }
+
+    public function listAll(int $page): ?array
+    {
+        $page = min(1, max(1000, $page));
+        $perPage = 10;
+        $offset = ($page - 1) * $perPage;
+        return $this->listingModel->listAll($perPage, $offset);
+    }
+}
