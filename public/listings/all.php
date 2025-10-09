3f3d05c73d67230a6b35a2a895dff5acc58e608c
feiMopps <132297930+feiMopps@users.noreply.github.com>
2025-10-10 01:58:24 +0200

    all listings page

--- public/listings/all.php
+++ public/listings/all.php
@@ -13,14 +13,17 @@ if (isset($_SERVER['HTTP_HX_REQUEST'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/_css/listings.css">
     <script src="/_js/htmx.min.js"></script>
     <title>Oferty</title>
 </head>
 <body>
     <h1>Aktualne oferty</h1>
     <div id="listings-outer">
-        <div id="aei"></div>
-        <button hx-get="?page=<?= max($_GET['page'] ?: 1, 1) ?>" hx-target="#aei">ae</button>
+        <div id="offers">
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/../resources/hx-templates/listings.php'; ?>
+        </div>
+        <span class=""></span>
     </div>
 </body>
 
--- resources/hx-templates/listings.php
+++ resources/hx-templates/listings.php
@@ -3,5 +3,24 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
 $listing = (new App\Builder\ListingBuilder())->make();
 
-$page = $_GET['page'] ?: 1;
-var_dump($listing->listAll($page));
+$page = max($_GET['page'] ?: 1, 1);
+?>
+
+
+
+<?php foreach($listing->listAll($page) as $lis): ?>
+<article>
+    <main>
+        <h2><?= $lis['title'] ?></h2>
+        <p><?= $lis['description'] ?></p>
+    </main>
+    <footer>
+        <hr>
+        <h2><?= $lis['price'] ?></h2>
+    </footer>
+</article>
+<?php endforeach; ?>
+<?php if (!empty($listing->listAll($page + 1))): ?>
+<div class="target" aria-hidden="true" hx-get="?page=<?= $page + 1 ?>" hx-target="this" hx-swap="outerHTML" hx-trigger="revealed"></div>
+<?php endif; ?>
+
