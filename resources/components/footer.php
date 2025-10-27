59ac22c7d5a85c5011e923951a91d57256206a11
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-10-27 12:57:16 +0100

    Footer responsiveness

--- resources/components/footer.php
+++ resources/components/footer.php
@@ -1,14 +1,15 @@
 <footer>
-    <ul>
-        <li><h1>Nawigacja</h1></li>
-        <li><a href="/">Strona główna</a></li>
-        <li><a href="/listings/all.php">Ogłoszenia</a></li>
-    </ul>
+    <div class="footer-inner">
+        <ul>
+            <li><h1>Nawigacja</h1></li>
+            <li><a href="/">Strona główna</a></li>
+            <li><a href="/listings/all.php">Ogłoszenia</a></li>
+        </ul>
 
-    <ul>
-        <li><h1>Przydatne linki</h1></li>
-        <li><a href="/terms-of-use">Regulamin</a></li>
-        <li><a href="/privacy-policy">Polityka prywatności</a></li>
-    </ul>
-
+        <ul>
+            <li><h1>Przydatne linki</h1></li>
+            <li><a href="/terms-of-use">Regulamin</a></li>
+            <li><a href="/privacy-policy">Polityka prywatności</a></li>
+        </ul>
+    </div>
 </footer>
