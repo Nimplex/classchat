5c25feda71118589f6517ea57b96054bda37414e
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-12-01 00:39:21 +0100

    Add favicon

--- resources/components/container.php
+++ resources/components/container.php
@@ -27,13 +27,21 @@ if (!in_array($lang, $allowed, true)) {
     <title><?= $title ?? 'Helix enthusiasts club' ?> | Szopex</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preload" href="/_dist/css/base.css" as="style">
+
     <?php if (!isset($no_navbar)): ?>
-    <link rel="preload" href="/_dist/js/navbar.js" as="script">
+        <link rel="preload" href="/_dist/js/navbar.js" as="script">
     <?php endif; ?>
+
     <link rel="preload" href="https://rsms.me/inter/inter.css" as="style">
     <link rel="preconnect" href="https://rsms.me/">
     <link rel="stylesheet" href="/_dist/css/base.css">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="icon" type="image/png" href="/_assets/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/_assets/favicon.svg" />
+    <link rel="shortcut icon" href="/_assets/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/_assets/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Szopex" />
+    <link rel="manifest" href="/_assets/site.webmanifest" />
     <script type="module" src="/_dist/js/icons.js"></script>
     <?php if (isset($render_head)) {
         echo $render_head();
@@ -58,7 +66,7 @@ if (!in_array($lang, $allowed, true)) {
     } ?>
 
     <?php if (!isset($no_navbar)): ?>
-    <script type="module" src="/_dist/js/navbar.js"></script>
+        <script type="module" src="/_dist/js/navbar.js"></script>
     <?php endif; ?>
 
     <?php require $_SERVER['DOCUMENT_ROOT'] . '/../resources/components/footer.php'; ?>
