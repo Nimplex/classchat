0d3b0be2cf1c1037b45f7c445af6b2e5c8d9d512
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-12-08 13:00:48 +0100

    Allow for non-tls smtp server

--- src/Mailer.php
+++ src/Mailer.php
@@ -16,13 +16,16 @@ class Mailer
         $this->mail = new PHPMailer(true);
         $this->mail->isSMTP();
         $this->mail->Host = $_ENV['SMTP_HOST'];
-        $this->mail->SMTPAuth = true;
+        $this->mail->SMTPAuth = isset($_ENV['SMTP_PASS']);
         $this->mail->Username = $_ENV['SMTP_USER'];
         $this->mail->Password = $_ENV['SMTP_PASS'];
-        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $this->mail->Port = 587;
+        if (isset($_ENV['SMTP_TLS'])) {
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        }
+        $this->mail->Port = isset($_ENV['SMTP_TLS']) ? 587 : 25;
         $this->mail->setFrom($_ENV['SMTP_USER'], 'Shopex');
         $this->mail->isHTML(true);
+        $this->mail->SMTPKeepAlive = true;
     }
 
     public function send(string $to, string $subject, string $template, array $vars = []): bool
